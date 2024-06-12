fe04c734b67bca443d606019e688250cae30d8d2
achen443 <achen443@gatech.edu>
2024-06-12 04:27:51 -0400

    began routes in client. Check the player route again in the morning

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { response } from 'express';
 
 function App() {
   const [servers, setServers] = useState([]);
@@ -15,12 +16,37 @@ function App() {
       });
   }, []);
 
+  useEffect(() => {
+    servers.forEach(server => {
+      axios.get(`http://localhost:3001/api/servers/:ip/players`)
+        .then(response => {
+          setPlayers(prevPlayers => ({
+            ...prevPlayers,
+            [server.addr]: response.data,
+          }));
+        })
+        .catch(error => {
+          console.error("Error getting server players:", error);
+        });
+    });
+  }, [servers]);
+
   return (
     <div className="App">
       <h1>Steam Servers</h1>
       {error && <p>Error: {error}</p>}
       <ul>
-        {servers}
+        {servers.map((server, index) => (
+          <li key={index}>
+            <p><strong>Address:</strong> {server.addr}</p>
+            <p><strong>Gameport:</strong> {server.gameport}</p>
+            <p><strong>Name:</strong> {server.name}</p>
+            <p><strong>Region:</strong> {server.region}</p>
+            <p><strong>Players:</strong> {server.players}/{server.max_players}</p>
+            <p><strong>Map:</strong> {server.map}</p>
+            <p><strong>Gametype:</strong> {server.gametype}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
